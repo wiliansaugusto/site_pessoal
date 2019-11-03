3a56f3392f7463352e8c74892add4ae54cc45b2e
wiliansaugusto <wiliansaugusto@gmail.com>
2019-11-03 11:53:29 -0300

    teste

--- navegador.php
+++ navegador.php
@@ -14,7 +14,7 @@
       </li>
       <li class="nav-item dropdown navegador">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Desenvolvimento Web 
+          Desenvolvimento Web - mudei aqui
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="topicos_especiais.php">Tópicos Especiais em Informática</a>
